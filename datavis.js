b0d39eddae79fd66bc4646fc8281bb27c8e1e746
djaspurg12 <djaspurg12@earlham.edu>
2015-12-07 01:39:47 -0500

    Update datavis.js

--- datavis.js
+++ datavis.js
@@ -5,40 +5,87 @@ var selected_marker = null;
 var mapInit = function() {
 
     //center map on indiana 
-    var mapProps = {
-    center: {lat: 40.0000, lng: -86.0000},
-    zoom: 7
-    panControl:true,
-    zoomControl:true,
-    mapTypeControl:true,
-    scaleControl:true,
-    streetViewControl:true,
-    overviewMapControl:true,
-    rotateControl:true, 
-    mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-    //instantiate the map
-    HazmatMap = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-var cities = [
-	      ['Ft. Wayne',41.0804, -85.1392 ,4],
-	      ['Indianapolis',39.7910, -86.1480 ,5],
-	      ['Marion',40.5492, -85.6647 ,3],
-	      ['Bloomington', 39.1622, -86.5292 ,2],
-	      ['Muncie', 40.1933, -85.3881, 1]
-	      ];
-
-funtion setMarkers(map){
-    for (var i = 0; i < cities.length; i++) {
-	var beach = cites[i];
-	var marker = new google.maps.Marker({
-		position: {lat: cities[1], lng: cities[2]},
-		map: map,,
-		title: cities[0],
-		zIndex: cities[3]
-	    });
-    }
-	      
-var infoWindow = new google.maps.InfoWindow({map: map});
+    center: new google.maps.LatLng(40.027072,-85.449962),
+            zoom: 6,
+            zoomControl: true,
+            zoomControlOptions: {
+                style: google.maps.ZoomControlStyle.DEFAULT,
+            },
+            disableDoubleClickZoom: true,
+            mapTypeControl: true,
+            mapTypeControlOptions: {
+                style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+            },
+            scaleControl: true,
+            scrollwheel: true,
+            panControl: true,
+            streetViewControl: true,
+            draggable : true,
+            overviewMapControl: true,
+            overviewMapControlOptions: {
+                opened: false,
+            },
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+        }
+        var mapElement = document.getElementById('Hazmat');
+        var map = new google.maps.Map(mapElement, mapOptions);
+        var locations = [
+		['Ft. Wayne, IN', 'undefined', 'undefined', 'undefined', 'undefined', 41.0804,  -85.1392],
+		['Indianapolis, IN', 'undefined', 'undefined', 'undefined', 'undefined', 39.7910,  -86.1480],
+		['Marion, IN', 'undefined', 'undefined', 'undefined', 'undefined', 40.5492,  -85.6647],
+		['Bloomington, IN', 'undefined', 'undefined', 'undefined', 'undefined', 39.1622,  -86.5292],
+		['Muncie, IN', 'undefined', 'undefined', 'undefined', 'undefined', 40.1933,  -85.3881]
+        ];
+        for (i = 0; i < locations.length; i++) {
+			if (locations[i][1] =='undefined'){ description ='';} else { description = locations[i][1];}
+			if (locations[i][2] =='undefined'){ telephone ='';} else { telephone = locations[i][2];}
+			if (locations[i][3] =='undefined'){ email ='';} else { email = locations[i][3];}
+           if (locations[i][4] =='undefined'){ web ='';} else { web = locations[i][4];}
+           if (locations[i][7] =='undefined'){ markericon ='';} else { markericon = locations[i][7];}
+            marker = new google.maps.Marker({
+                icon: markericon,
+                position: new google.maps.LatLng(locations[i][5], locations[i][6]),
+                map: map,
+                title: locations[i][0],
+                desc: description,
+                tel: telephone,
+                email: email,
+                web: web
+            });
+if (web.substring(0, 7) != "http://") {
+link = "http://" + web;
+} else {
+link = web;
+}
+            bindInfoWindow(marker, map, locations[i][0], description, telephone, email, web, link);
+     }
+ function bindInfoWindow(marker, map, title, desc, telephone, email, web, link) {
+      var infoWindowVisible = (function () {
+              var currentlyVisible = false;
+              return function (visible) {
+                  if (visible !== undefined) {
+                      currentlyVisible = visible;
+                  }
+                  return currentlyVisible;
+               };
+           }());
+           iw = new google.maps.InfoWindow();
+           google.maps.event.addListener(marker, 'click', function() {
+               if (infoWindowVisible()) {
+                   iw.close();
+                   infoWindowVisible(false);
+               } else {
+                   var html= "<div style='color:#000;background-color:#fff;padding:5px;width:150px;'><h4>"+title+"</h4><p>"+desc+"<p><p>"+telephone+"<p><a href='mailto:"+email+"' >"+email+"<a><a href='"+link+"'' >"+web+"<a></div>";
+                   iw = new google.maps.InfoWindow({content:html});
+                   iw.open(map,marker);
+                   infoWindowVisible(true);
+               }
+        });
+        google.maps.event.addListener(iw, 'closeclick', function () {
+            infoWindowVisible(false);
+        });
+ }
+}
 
   //  HTML5 geolocation.
   if (navigator.geolocation) {
